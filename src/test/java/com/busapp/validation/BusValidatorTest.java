d2522b37b72503477bb2c701d78ca2f32ab53603
Vadim Tryushnikov <random@mail.ru>
2025-12-12 01:23:04 +0500

    bugfix in Bus, BusList, Builder

--- src/test/java/com/busapp/validation/BusValidatorTest.java
+++ src/test/java/com/busapp/validation/BusValidatorTest.java
@@ -13,10 +13,10 @@ public class BusValidatorTest {
                 .setNext(new BussModelValidator())
                 .setNext(new BussNumberValidator());
 
-        Bus builder = new Bus()
-                .setNumber("А123БВ")
-                .setModel("МАЗ")
-                .setMileage(50000);
+        Bus.Builder builder = new Bus.Builder()
+                .number("А123БВ")
+                .model("МАЗ")
+                .mileage(50000);
 
         assertTrue(() ->
                 validatorChain.validate(builder.build()).status() == BusValidator.ValidationStatus.SUCCESS);
@@ -29,10 +29,11 @@ public class BusValidatorTest {
                 .setNext(new BussModelValidator())
                 .setNext(new BussNumberValidator());
 
-        Bus invalidBus = new Bus()
-                .setNumber("РТ678СО")
-                .setModel("MERZ")
-                .setMileage(-90);
+        Bus invalidBus = new Bus.Builder()
+                .number("РТ678СО")
+                .model("MERZ")
+                .mileage(-90)
+                .build();
 
         assertTrue(() ->
                 validatorChain.validate(invalidBus).status() == BusValidator.ValidationStatus.FAIL);
